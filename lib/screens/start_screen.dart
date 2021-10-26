de5378923f2418adca4079b2f281bfe53c556136
robinhood <robinhood@mail.com>
2021-10-26 18:46:46 +0530

    core error solved

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -3,9 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'screens.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+//import 'package:google_sign_in/google_sign_in.dart';
 //import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase/firebase.dart';
+//import 'package:firebase/firebase.dart';
 
 class StartScreen extends StatefulWidget {
   @override
